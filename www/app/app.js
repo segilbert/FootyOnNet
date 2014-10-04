8f473a65e4e3687b06365eb415fd07dcb6cbc7fe
Sean Gilbert <getonthetrail@gmail.com>
2014-10-04 10:51:46 -0400

    add pages for footer navigations

--- www/app/app.js
+++ www/app/app.js
@@ -19,8 +19,36 @@ angular.module("footyNoNetApp", ["ionic"])
   $stateProvider
 
     .state('home', {
+      abstract: true,
       url: "/home",
       templateUrl: "app/home/home.html"
+    })
+
+ 	.state('home.leagues', {
+      url: "/leagues",
+      views: {
+        "tab-leagues": {
+          templateUrl: "app/home/leagues.html"
+        }
+      }
+    })
+
+	.state('home.footyboard', {
+      url: "/footyboard",
+      views: {
+        "tab-footyboard": {
+          templateUrl: "app/home/footyboard.html"
+        }
+      }
+    })
+
+    .state('home.myteams', {
+      url: "/myteams",
+      views: {
+        "tab-myteams": {
+          templateUrl: "app/home/myteams.html"
+        }
+      }
     })
 
     .state('app', {
@@ -29,5 +57,5 @@ angular.module("footyNoNetApp", ["ionic"])
     })
 
     // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app');
+  $urlRouterProvider.otherwise('/home/leagues');
 });
